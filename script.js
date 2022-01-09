31db211902d52e83ca21114c54942b33548bfd56
Sandrajasmin <sandra-jasmin@hotmail.com>
2022-01-09 21:44:30 +0100

    removed comments

--- script.js
+++ script.js
@@ -11,13 +11,7 @@ console.log("The height is" + " " + integer + "m");
 //Boolean value
 console.log(mountain < integer);
 
-/*TASK TWO. Declare two variables. 
-Assign your first name to one of the variables 
-and your last name to the other. 
-Concatenate (join) the variables, 
-assign them to a third variable and 
-console log the value of the third variable.*/
-
+//TASK TWO. 
 
 var firstName = "Sandra";
 var lastName = "Møller-Sørensen";
@@ -29,10 +23,7 @@ var nameType = typeof (fullName);
 console.log(typeof (fullName));
 
 
-/*TASK THREE. Check the type of the value "frog"
- with the typeof operator and assign the result to a variable. 
- Console log the variable value with a label, e.g. 
- “The type of frog is ___”.*/
+//TASK THREE
 
 var animal = "frog";
 console.log(typeof (animal));
@@ -41,24 +32,16 @@ var animalType = typeof (crazyfrog);
 var animalType = "The type of frog is crazyfrog";
 console.log(animalType);
 
-/*4. Declare and initialise a variable called orderHasShipped 
-with a boolean value. Create an if statement that checks if 
-orderHasShipped is true.
-If it is true, console log the string value "The order shipped".
- If not, console log the string value "The order did not ship". */
+// TASK FOUR 
 
 var orderHasShipped = true;
 
-if(orderHasShipped === true) {
+if (orderHasShipped === true) {
     console.log("the order shipped");
 }
 
-/*5. Create a for loop that counts from 7 to 13. 
-Console log the value of the counter variable inside the loop. */
+//TASK FIVE
 
-for(var count = 7; count <= 13; count++) {
+for (var count = 7; count <= 13; count++) {
     console.log(count);
 }
-
-
-
